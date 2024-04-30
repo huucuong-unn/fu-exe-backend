26d569f9737c10d4d515625d4eeedbaa61e80479
CuongLH <lehuucuong270603@gmail.com>
2024-04-30 18:14:10 +0700

    test github actions

--- backend/src/main/java/com/exe01/backend/constant/ConstAPI.java
+++ backend/src/main/java/com/exe01/backend/constant/ConstAPI.java
@@ -4,6 +4,8 @@ public class ConstAPI {
     public class AuthenticationAPI {
         public static final String LOGIN_WITH_PASSWORD_USERNAME = "api/v1/authentication/login";
 
+        public static final String TEST_AWS_DEPLOY = "api/v1/authentication/test-aws-deploy";
+
     }
 
 
--- backend/src/main/java/com/exe01/backend/controller/UserController.java
+++ backend/src/main/java/com/exe01/backend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.exe01.backend.controller;
 import com.exe01.backend.constant.ConstAPI;
 import com.exe01.backend.dto.UserDTO;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @PostMapping(value = ConstAPI.AuthenticationAPI.LOGIN_WITH_PASSWORD_USERNAME)
-    public UserDTO login(){
-        return  null;
+    public String login(){
+        return  "hello";
+    }
+
+    @GetMapping(value = ConstAPI.AuthenticationAPI.TEST_AWS_DEPLOY)
+    public String testAwsDeploy(){
+        return  "hello test aws deploy";
     }
 }
