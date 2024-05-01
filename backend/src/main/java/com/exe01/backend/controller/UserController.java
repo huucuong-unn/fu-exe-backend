6060f8e948c683886735b72c5a135a36f72093c6
CuongLH <lehuucuong270603@gmail.com>
2024-05-01 16:00:37 +0700

    test another text

--- backend/src/main/java/com/exe01/backend/controller/UserController.java
+++ backend/src/main/java/com/exe01/backend/controller/UserController.java
@@ -21,6 +21,8 @@ public class UserController {
         return  "hello test aws deploy";
     }    @GetMapping(value = ConstAPI.AuthenticationAPI.TEST_AWS_DEPLOY2)
     public String testAwsDeploy2(){
-        return  "Hello út be khùm";
+        return  " /\\_/\\  \n" +
+                "( o.o ) \n" +
+                " > ^ <";
     }
 }
