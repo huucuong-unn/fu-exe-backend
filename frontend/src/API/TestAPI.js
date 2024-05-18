f6460de98ac96dbc064d10e4f6adf6e4a0c278f4
CuongLH <lehuucuong270603@gmail.com>
2024-05-18 12:26:13 +0700

    FE: change api link

--- frontend/src/API/TestAPI.js
+++ frontend/src/API/TestAPI.js
@@ -1,7 +1,7 @@
-const { default: axiosClient } = require("./AxiosClient");
+const { default: axiosClient } = require('./AxiosClient');
 
 const testAPI = async () => {
-    const response = await axiosClient.get('/v1/authentication/test-aws-deploy2');
+    const response = await axiosClient.get('/v1/auth/test-aws-deploy2');
     return response;
 };
 
