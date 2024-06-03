7705aeaab09ede202d660450b6307291712571dd
nguyenthanh311003 <nguyenthanh311003@gmail.com>
2024-06-03 15:56:14 +0700

    [ThanhNT][11-TWELL-3-06-2024][DEVELOP]: update UniversityServiceImpl and StudentServiceImpl

--- src/main/java/com/exe01/backend/dto/StudentDTO.java
+++ src/main/java/com/exe01/backend/dto/StudentDTO.java
@@ -1,26 +1,18 @@
 package com.exe01.backend.dto;
 
-import com.exe01.backend.entity.Account;
-import com.exe01.backend.entity.University;
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudentDTO extends BaseDTO implements Serializable {
-
 
     private String name;
 
