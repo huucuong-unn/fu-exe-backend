74699fb58a3c9356d802e622355d7b084fa1bc50
VinhLCV <congvinhdt1223@gmail.com>
2024-07-16 13:19:58 +0700

    Notification

--- src/main/java/com/exe01/backend/controller/NotificationController.java
+++ src/main/java/com/exe01/backend/controller/NotificationController.java
@@ -1,23 +1,29 @@
-//package com.exe01.backend.controller;
-//
-//import com.exe01.backend.constant.ConstAPI;
-//import com.exe01.backend.dto.response.NotificationMessage;
-//import com.exe01.backend.service.impl.FirebaseMessagingService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class NotificationController {
-//
-//    @Autowired
-//    FirebaseMessagingService firebaseMessagingService;
-//
-//    @PostMapping(value = ConstAPI.NotificationAPI.SEND_NOTIFITION)
-//    public String sendNotificationByToken(@RequestBody NotificationMessage notificationMessage)
-//    {
-//        return firebaseMessagingService.sendNotificationByToken(notificationMessage);
-//    }
-//
-//}
+package com.exe01.backend.controller;
+
+
+import com.exe01.backend.service.impl.FirebaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+
+    @Autowired
+    private FirebaseService firebaseService;
+
+    @PostMapping("/send")
+    public String sendNotification(@RequestBody Map<String, String> request) {
+        String token = request.get("token");
+        String title = request.get("title");
+        String body = request.get("body");
+
+        return firebaseService.sendNotification(token, title, body);
+    }
+}
+
