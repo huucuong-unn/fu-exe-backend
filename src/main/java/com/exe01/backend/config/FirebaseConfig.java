c7f9796a02b24406b0a7b54f7e1ad6b282ed4996
VinhLCV <congvinhdt1223@gmail.com>
2024-07-16 14:42:29 +0700

    fix dockerv3

--- src/main/java/com/exe01/backend/config/FirebaseConfig.java
+++ src/main/java/com/exe01/backend/config/FirebaseConfig.java
@@ -1,4 +1,5 @@
 package com.exe01.backend.config;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -6,23 +7,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class FirebaseConfig {
 
-    private String serviceAccountKeyPath = "src/main/resources/firebase-service-account.json";
+    @Value("${firebase.string.config}")
+    private String serviceAccount;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream(serviceAccountKeyPath);
-
-        // Load JSON file into FirebaseOptions
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(serviceAccount.getBytes())))
                 .build();
 
         return FirebaseApp.initializeApp(options);
